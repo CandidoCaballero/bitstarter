22f53675a6d2373588581188b0547c09881bbc3a
CandidoCaballero <candido.caballero@gmail.com>
2013-07-10 08:39:20 +0000

    subido checks.json grader.js web.js

--- web.js
+++ web.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  
 var fs = require('fs');
 var f=fs.readFileSync('./index.html');
 var buffer = new Buffer(f);
